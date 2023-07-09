91fabf2dba6d69bf4576e7ba7243fd5ac11b1f4e
Petar Nikov <nexusstar@users.noreply.github.com>
2023-07-09 08:51:43 +0200

    add python

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,11 +16,12 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- tailwind css,
     { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.test.core" },
 
