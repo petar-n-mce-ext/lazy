c90497702bbac8ec2fc7087618696c303baa000e
Petar Nikov <nexusstar@users.noreply.github.com>
2023-07-18 22:21:47 +0200

    fix: some things get lost so returning them

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,3 +17,5 @@ end
 map("i", "<C-u>", "<Esc>gUiw`]a", { desc = "To uppercase" })
 map("n", "<C-d>", "<C-d>zz", { desc = "Half page down" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Half page up" })
+map("n", "n", "nzzzv", { desc = "Search forward" })
+map("n", "N", "Nzzzv", { desc = "Search backwards" })
